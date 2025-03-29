cf98554c5a05eb7c48d5eea79d54862ecf7caa53
Abhi-atg-1021 <abhisinghal112@gmail.com>
2025-03-30 00:47:16 +0530

    lets see

--- portfolio_builder/vite.config.ts
+++ portfolio_builder/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensure this is correctly set
+  build: {
+    outDir: "dist", // Ensure this matches the staticPublishPath in render.yaml
+  }
 })
